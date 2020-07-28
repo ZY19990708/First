65b7d205e0f4bed5387852e980be8df26c3ea566
lzp <lzp@lzp-VirtualBox.(none)>
2020-07-28 15:39:56 +0800

    学生

---
+++ student.h
@@ -0,0 +1,26 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <getch.h>
+typedef struct Student
+{
+	char name[100][20];
+	char sex[100];
+	int ID[100];
+	char pw[100][10];
+	int pd[100];
+	int Chinese[100];
+	int Math[100];
+	int English[100];
+}Student;
+//查排名
+void find(int num);
+
+//修改密码
+void cpd(int num);
+
+//查看个人信息
+void look(int num);
+
+//进入学生界面
+void run_stu(void);
+#endif// STUDENT_H
